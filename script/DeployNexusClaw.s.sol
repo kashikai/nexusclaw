35cd45228bf2303721cd1adfbdac90d3cd0aabc5
Tiago <tiago@nexusclaw.ai>
2026-03-24 21:40:26 +0900

    feat: NexusClaw v2 deployed and verified on Base Sepolia - 0x502C37f56CC77F9455490c28a45a34bED225D110

--- script/DeployNexusClaw.s.sol
+++ script/DeployNexusClaw.s.sol
@@ -1,22 +1,20 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
+pragma solidity ^0.8.24;
 import {Script, console} from "forge-std/Script.sol";
 import {NexusClaw} from "../src/NexusClaw.sol";
 
 contract DeployNexusClaw is Script {
     function run(address treasury) external returns (NexusClaw) {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        vm.startBroadcast(deployerPrivateKey);
+ 
 
+       vm.startBroadcast(deployerPrivateKey);
         NexusClaw claw = new NexusClaw(treasury);
         console.log("NexusClaw deployed to:", address(claw));
         console.log("Treasury balance:", claw.balanceOf(treasury));
         console.log("Total supply:", claw.totalSupply());
-        console.log("Burn fee enabled:", claw.burnFeeEnabled());
-
+        console.log("Contract balance:", claw.balanceOf(address(claw)));
         vm.stopBroadcast();
-
         return claw;
     }
 }
