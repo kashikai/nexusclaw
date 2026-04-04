b75dbc76dfd7f831aea239a15f521333b3ad442d
Tiago <tiyabe01@gmail.com>
2026-04-05 01:11:28 +0900

    fix(frontend): use ESM export default in next.config.js

--- frontend-v2/next.config.js
+++ frontend-v2/next.config.js
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
       encoding: false,
-    }
-    return config
+    };
+    return config;
   },
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
